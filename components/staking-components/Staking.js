c53d93611babc233cab8420051dfb31927d9e9cd
AlexCPT-cpu <67722561+AlexCPT-cpu@users.noreply.github.com>
2022-12-21 03:11:40 +0100

    Add files via upload

---
+++ components/staking-components/Staking.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Header from '../Header'
+import Nav from '../Nav'
+import StakedPagination from './StakedPagination'
+import StakingDetails from './StakingDetails'
+import UnstakedPagination from './UnstakedPagination'
+import { useAddress } from '@thirdweb-dev/react'
+
+
+const Staking = ({ name, user, vaultId, contract, cardName }) => {
+  
+  return (
+    <div className="bg-[#0d0613] min-h-screen flex flex-col">
+      <Header name={name} />
+      <StakingDetails user={user} vaultId={0} />
+          <UnstakedPagination vaultId={0} contract={contract} cardName={cardName} />
+        <StakedPagination vaultId={0} contract={contract} cardName={cardName} />
+      </div>
+  )
+}
+
+export default Staking
---
+++ pages/lotteries/Hac333.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Lottery from '../../components/lottery-components/Lottery'
+import { useContract, useContractRead, useAddress } from "@thirdweb-dev/react";
+
+
+const Hac333 = () => {
+
+  const address = useAddress()
+
+  const { contract } = useContract("0x62937Ef07743c40cE212DD85084f6C2a00E9C271");
+  const { contract: tokenContract } = useContract("0xb6b4FFF2e95aAdd0B08fe1Cc087Efb5cF0a679D8");
+  const { data: owner } = useContractRead(contract, "owner")
+
+  return (
+    <div className="bg-[#0d0613] min-h-screen flex flex-col">
+      <Lottery admin={owner} contract={contract} name='333' token={tokenContract} />
+      </div>
+  )
+}
+
+export default Hac333
