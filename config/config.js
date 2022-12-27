15f62c1dbd7e11790adb2816cbc06e00657ace63
AlexCPT-cpu <67722561+AlexCPT-cpu@users.noreply.github.com>
2022-12-27 16:32:17 +0100

    Add files via upload

---
+++ config/config.js
@@ -0,0 +1,4 @@
+export const _333contract = '0xD84d651e4B6968FF8865cFacAF3db20b28b136a8'
+export const _3333contract = '0xD84d651e4B6968FF8865cFacAF3db20b28b136a8'
+export const alienContract = '0xD84d651e4B6968FF8865cFacAF3db20b28b136a8'
+export const vaultContract = '0x76F69BE8739b4B0D8A23c498b880614aA01bA91B'
