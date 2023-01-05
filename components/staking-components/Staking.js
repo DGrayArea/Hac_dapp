f5508bdfe15a4d62c7f62404d28b2a1058cfa9e8
AlexCPT-cpu <67722561+AlexCPT-cpu@users.noreply.github.com>
2023-01-05 03:14:25 +0100

    Add files via upload

---
+++ components/staking-components/Staking.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from '../Header'
+import StakedPagination from './StakedPagination'
+import StakingDetails from './StakingDetails'
+import UnstakedPagination from './UnstakedPagination'
+
+
+const Staking = ({ name, user, vaultId, contract, cardName, token, vaultC }) => {
+  
+  return (
+    <div className="bg-black min-h-screen flex flex-col">
+      <Header name={name} />
+      <StakingDetails token={token} user={user} vaultId={vaultId} />
+          <UnstakedPagination vaultId={vaultId} contract={contract} cardName={cardName} />
+        <StakedPagination vaultC={vaultC} vaultId={vaultId} contract={contract} cardName={cardName} />
+      </div>
+  )
+}
+
+export default Staking
