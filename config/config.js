4f2682fad0a317d8fb63f4ddeeb0454f7478bc8b
AlexCPT-cpu <67722561+AlexCPT-cpu@users.noreply.github.com>
2023-01-08 01:48:34 +0100

    Delete config directory

--- config/config.js
+++
@@ -1,4 +0,0 @@
-export const _333contract = '0x7E82F54578f03ad922F2bA61674C46b9dBdCE61d'
-export const _3333contract = '0xAFc7647b584730694B9606511F11F423A0816eFf'
-export const alienContract = '0xE33cc98d90975Ad42e56251B0A1cd8bda9FAF003'
-export const vaultContract = '0x4d2Bfa7d1D13e6C13A08a92B32a238b2B6DFFE0E'
