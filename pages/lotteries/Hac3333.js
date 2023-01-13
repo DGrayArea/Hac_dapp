a64f9556017b845f70e62b0a8a6f77b207d2b48f
AlexCPT-cpu <67722561+AlexCPT-cpu@users.noreply.github.com>
2023-01-13 10:14:14 +0100

    Update Hac3333.js

--- pages/lotteries/Hac3333.js
+++ pages/lotteries/Hac3333.js
@@ -11,7 +11,7 @@ const Hac3333 = () => {
 
   return (
     <div className="bg-black min-h-screen flex flex-col">
-      <Lottery endTime={'Jan 13, 2023 22:00:00'} tokenString={'0x38a5100E72805F35F7D014d2Ef7c69e8D24c6D81'} tContract={'0xD48c2F6Cd0554b32C90E70C4C493Ce253299dC68'} admin={owner} contract={contract} name='HORROR APES' token={tokenContract} />
+      <Lottery endTime={'Jan 13, 2023 23:00:00'} tokenString={'0x38a5100E72805F35F7D014d2Ef7c69e8D24c6D81'} tContract={'0xD48c2F6Cd0554b32C90E70C4C493Ce253299dC68'} admin={owner} contract={contract} name='HORROR APES' token={tokenContract} />
       </div>
   )
 }
