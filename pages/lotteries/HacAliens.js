e569d2291d8e4a82e07daccc241e6ced6761ade4
AlexCPT-cpu <67722561+AlexCPT-cpu@users.noreply.github.com>
2023-01-13 10:14:37 +0100

    Update HacAliens.js

--- pages/lotteries/HacAliens.js
+++ pages/lotteries/HacAliens.js
@@ -11,7 +11,7 @@ const HacAliens = () => {
 
   return (
     <div className="bg-black min-h-screen flex flex-col">
-      <Lottery endTime={'Jan 27, 2023 22:00:00'} tokenString={'0x760ED02d9407372416155A5962092ba95ebc1290'} tContract={'0x120A9664D35031691eC7ee98B81F1A87ebbD1217'} admin={owner} contract={contract} name='ALIEN HORROR APES' token={tokenContract} />
+      <Lottery endTime={'Jan 27, 2023 23:00:00'} tokenString={'0x760ED02d9407372416155A5962092ba95ebc1290'} tContract={'0x120A9664D35031691eC7ee98B81F1A87ebbD1217'} admin={owner} contract={contract} name='ALIEN HORROR APES' token={tokenContract} />
       </div>
   )
 }
